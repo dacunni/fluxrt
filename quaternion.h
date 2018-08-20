5870272e0f796f9bb9983a27f06e90521e03e6cd
David Cunningham <dacunni@gmail.com>
2018-08-19 20:57:58 -0600

    Add quat multiplication and conjugate

--- quaternion.h
+++ quaternion.h
@@ -5,27 +5,24 @@
 
 struct quaternion 
 {
-	inline quaternion() = default;
-	inline quaternion(const quaternion & a);
-	inline quaternion(float x, float y, float z, float r = 1.0f);
-	inline ~quaternion() {}
-	
+    inline quaternion() = default;
+    inline quaternion(const quaternion & a);
+    inline quaternion(float x, float y, float z, float r = 1.0f);
+    inline quaternion(const vec3 & v, float r = 1.0f);
+    inline ~quaternion() = default;
+
     inline void set(float x, float y, float z, float r);
 
-    // IMPLEMENT ME
-    inline quaternion conjugate();
-    
     float x = 0.0f;
     float y = 0.0f;
     float z = 0.0f;
     float r = 1.0f;
 };
 
-// quaternion math functions
-//   Includes in-place and algebraic forms for convenience
-inline void       mult(const quaternion & q, const quaternion & p, quaternion & r);
+inline float norm(const quaternion & q);
+inline quaternion conjugate(const quaternion & q);
+
 inline quaternion mult(const quaternion & q, const quaternion & p);
-inline void       rotate(const quaternion & q, const vec3 & v, vec3 & r);
 inline quaternion rotate(const quaternion & q, const vec3 & v);
 
 #include "quaternion.hpp"
--- quaternion.hpp
+++ quaternion.hpp
@@ -16,6 +16,11 @@ inline quaternion::quaternion(float xn, float yn, float zn, float rn)
     set(xn, yn, zn, rn);
 }
 
+inline quaternion::quaternion(const vec3 & v, float rn)
+{
+    set(v.x, v.y, v.z, rn);
+}
+
 inline void quaternion::set(float xn, float yn, float zn, float rn)
 {
 	x = xn;
@@ -24,21 +29,24 @@ inline void quaternion::set(float xn, float yn, float zn, float rn)
 	r = rn;
 }
 
-inline void mult(const quaternion & q, const quaternion & p, quaternion & r)
+inline float norm(const quaternion & q)
 {
-    // IMPLEMENT ME
+    return std::sqrt(sq(q.x) + sq(q.y) + sq(q.z) + sq(q.r));
+}
+
+inline quaternion conjugate(const quaternion & q)
+{
+    return quaternion(-q.x, -q.y, -q.z, q.r);
 }
 
 inline quaternion mult(const quaternion & q, const quaternion & p)
 {
-    quaternion r;
-    // IMPLEMENT ME
-    return r;
-}
-
-inline void rotate(const quaternion & q, const vec3 & v, vec3 & r)
-{
-    // IMPLEMENT ME
+    const float s1 = q.r;
+    const float s2 = p.r;
+    const vec3 v1 { q.x, q.y, q.z };
+    const vec3 v2 { p.x, p.y, p.z };
+    return quaternion(s1 * v2 + s2 * v1 + cross(v1, v2),
+                      s1 * s2 - dot(v1, v2));
 }
 
 inline quaternion rotate(const quaternion & q, const vec3 & v)
