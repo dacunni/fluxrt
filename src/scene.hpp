2c504ab6a06349216fa84bc4c34f0531c53ecf56
David Cunningham <dacunni@gmail.com>
2019-08-17 22:28:16 -0600

    Fixed distance test/update logic in ray-scene intersection

--- src/scene.hpp
+++ src/scene.hpp
@@ -30,34 +30,39 @@ inline bool intersects(const Ray & ray, const Scene & scene, float minDistance)
 inline bool findIntersection(const Ray & ray, const Scene & scene,
                              float minDistance, RayIntersection & intersection)
 {
-    bool hit = false;
     RayIntersection nextIntersection;
+    bool hit = false;
+
+    const auto updateBestHit = [&]() {
+        if(!hit || nextIntersection.distance < intersection.distance) {
+            intersection = nextIntersection;
+            hit = true;
+        }
+    };
+
+    // Iterate over scene objects
 
     for(const auto & o : scene.spheres) {
         if(findIntersection(ray, o, minDistance, nextIntersection)) {
-            intersection = nextIntersection;
-            hit = true;
+            updateBestHit();
         }
     }
 
     for(const auto & o : scene.slabs) {
         if(findIntersection(ray, o, minDistance, nextIntersection)) {
-            intersection = nextIntersection;
-            hit = true;
+            updateBestHit();
         }
     }
 
     for(const auto & o : scene.meshOctrees) {
         if(findIntersection(ray, o, minDistance, nextIntersection)) {
-            intersection = nextIntersection;
-            hit = true;
+            updateBestHit();
         }
     }
 
     for(const auto & o : scene.meshes) {
         if(findIntersection(ray, o, minDistance, nextIntersection)) {
-            intersection = nextIntersection;
-            hit = true;
+            updateBestHit();
         }
     }
 
