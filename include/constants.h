25530c10d1b0695efe6cff4082e42a5fdd944602
David Cunningham <dacunni@gmail.com>
2019-08-17 23:09:36 -0600

    Added more constants and radian to degree conversion

--- include/constants.h
+++ include/constants.h
@@ -5,10 +5,13 @@
 
 namespace constants {
 
-    static constexpr double PI = M_PI;
+    static constexpr double PI      = M_PI;
+    static constexpr double TWO_PI  = 2.0 * PI;
+    static constexpr double FOUR_PI = 4.0 * PI;
 
 };
 
 constexpr double DegreesToRadians(double d) { return d * constants::PI / 180.0; }
+constexpr double RadiansToDegrees(double r) { return r * 180.0 / constants::PI; }
 
 #endif
