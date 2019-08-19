89c1170858a70aa9ddef446b12a0b78d911da8d2
David Cunningham <dacunni@gmail.com>
2019-08-18 18:40:46 -0600

    Added Helmholtz reciprocity and positivity tests to BRDF unit tests

--- unittests/brdf.cpp
+++ unittests/brdf.cpp
@@ -7,6 +7,25 @@
 
 namespace {
 
+// Utility functions
+// TODO: This would be useful as a more general purpose function. Move it somewhere and clean it up.
+template<typename F>
+void forEachWiWoPairHemisphere(F f, unsigned int thetaSteps, unsigned int phiSteps)
+{
+    // We're just using integrate::overUnitHemisphere() to walk through theta/phi.
+    // Probably should just make a function that does this.
+    integrate::overUnitHemisphere(
+        [&](float theta1, float phi1) {
+            auto wi = Direction3(coordinate::polarToEuclidean(theta1, phi1, 1.0f));
+            return integrate::overUnitHemisphere(
+                [&](float theta2, float phi2) {
+                    auto wo = Direction3(coordinate::polarToEuclidean(theta2, phi2, 1.0f));
+                    f(wi, wo);
+                    return 0.0f; // dummy
+            }, thetaSteps, phiSteps);
+        }, thetaSteps, phiSteps);
+}
+
 //
 // Energy Conservation
 //
@@ -33,8 +52,33 @@ TEST(BrdfTest, Integrate_LambertianBrdfTimesProjSolidAngle_OverHemisphere_IsOne)
     EXPECT_NEAR(I, float(1.0), closeness);
 }
 
-// TODO: Helmholtz reciprocity
-// TODO: Positivity
+//
+// Positivity
+//
+
+TEST(BrdfTest, Positivity_LambertianBrdf) {
+    const unsigned int thetaSteps = 16, phiSteps = 32;
+    Direction3 N(0, 0, 1);
+    const float closeness = 0.01;
+
+    forEachWiWoPairHemisphere([&](const Direction3 & wi, const Direction3 & wo) {
+        EXPECT_TRUE(brdf::lambertian(wi, wo, N) >= 0.0f);
+    }, thetaSteps, phiSteps);
+}
+
+//
+// Helmholtz reciprocity
+//
+
+TEST(BrdfTest, HelmholtzReciprocity_LambertianBrdf) {
+    const unsigned int thetaSteps = 16, phiSteps = 32;
+    Direction3 N(0, 0, 1);
+    const float closeness = 0.01;
+
+    forEachWiWoPairHemisphere([&](const Direction3 & wi, const Direction3 & wo) {
+        EXPECT_NEAR(brdf::lambertian(wi, wo, N), brdf::lambertian(wo, wi, N), closeness);
+    }, thetaSteps, phiSteps);
+}
 
 } // namespace
 
