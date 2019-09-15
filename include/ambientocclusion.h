fe12963e40c785c46f4a6ea414a09c1eb3eb78a9
David Cunningham <dacunni@gmail.com>
2019-09-14 22:24:06 -0600

    Added ambient occlusion header

---
+++ include/ambientocclusion.h
@@ -0,0 +1,35 @@
+#ifndef __AMBIENTOCCLUSION_H__
+#define __AMBIENTOCCLUSION_H__
+
+#include "scene.h"
+#include "ray.h"
+#include "rng.h"
+
+float computeAmbientOcclusion(Scene & scene, const RayIntersection & intersection, float minDistance, RNG & rng,
+                              unsigned int numSamples, bool sampleCosineLobe)
+{
+    const float epsilon = 1.0e-4;
+    Position3 p = intersection.position + intersection.normal * epsilon;
+    Direction3 d;
+    float ao = 0.0f;
+    for(int i = 0; i < numSamples; ++i) {
+        if(sampleCosineLobe) {
+            // Sample according to cosine lobe about the normal
+            rng.cosineAboutDirection(intersection.normal, d);
+            Ray aoShadowRay(p, d);
+            ao += intersects(aoShadowRay, scene, minDistance) ? 0.0f : 1.0f;
+        }
+        else {
+            // Sample hemisphere and scale by cosine of angle to normal
+            rng.uniformSurfaceUnitHalfSphere(intersection.normal, d);
+            Ray aoShadowRay(p, d);
+            ao += intersects(aoShadowRay, scene, minDistance) ? 0.0f : 2.0f * dot(d, intersection.normal);
+        }
+    }
+    ao /= numSamples;
+
+    return ao;
+}
+
+
+#endif
--- tests/trace_scene.cpp
+++ tests/trace_scene.cpp
@@ -9,34 +9,9 @@
 #include "artifacts.h"
 #include "constants.h"
 #include "rng.h"
+#include "ambientocclusion.h"
 
 #include "argparse.h"
-
-float computeAmbientOcclusion(Scene & scene, const RayIntersection & intersection, float minDistance, RNG & rng,
-                              unsigned int numSamples, bool sampleCosineLobe)
-{
-    const float epsilon = 1.0e-4;
-    Position3 p = intersection.position + intersection.normal * epsilon;
-    Direction3 d;
-    float ao = 0.0f;
-    for(int i = 0; i < numSamples; ++i) {
-        if(sampleCosineLobe) {
-            // Sample according to cosine lobe about the normal
-            rng.cosineAboutDirection(intersection.normal, d);
-            Ray aoShadowRay(p, d);
-            ao += intersects(aoShadowRay, scene, minDistance) ? 0.0f : 1.0f;
-        }
-        else {
-            // Sample hemisphere and scale by cosine of angle to normal
-            rng.uniformSurfaceUnitHalfSphere(intersection.normal, d);
-            Ray aoShadowRay(p, d);
-            ao += intersects(aoShadowRay, scene, minDistance) ? 0.0f : 2.0f * dot(d, intersection.normal);
-        }
-    }
-    ao /= numSamples;
-
-    return ao;
-}
 
 int main(int argc, char ** argv)
 {
