1a69a54813c9317c14e4537fd3b1042650614212
David Cunningham <dacunni@gmail.com>
2019-09-19 21:59:34 -0600

    Cubemap environment maps

--- include/environmentmap.h
+++ include/environmentmap.h
@@ -2,8 +2,10 @@
 #define __ENVIRONMENT_MAP_H__
 
 #include "color.h"
+#include "texture.h"
 
 class Ray;
+class Direction3;
 
 class EnvironmentMap
 {
@@ -14,14 +16,37 @@ class EnvironmentMap
         virtual color::ColorRGB sampleRay(const Ray & ray);
 };
 
-class CubeMapEnvironmentMap
+class CubeMapEnvironmentMap : public EnvironmentMap
 {
     public:
         CubeMapEnvironmentMap() = default;
         virtual ~CubeMapEnvironmentMap() = default;
 
+        bool loadFromDirectionFiles(
+            const std::string & negx,
+            const std::string & posx,
+            const std::string & negy,
+            const std::string & posy,
+            const std::string & negz,
+            const std::string & posz);
 
         virtual color::ColorRGB sampleRay(const Ray & ray);
+
+    protected:
+        using TexturePtr = std::shared_ptr<Texture>;
+
+        TexturePtr loadDirectionTile(const std::string & file);
+
+        void directionToTileCoord(const Direction3 & v,
+                                  TexturePtr & texture,
+                                  TextureCoordinate & texcoord);
+
+        TexturePtr xn;
+        TexturePtr xp;
+        TexturePtr yn;
+        TexturePtr yp;
+        TexturePtr zn;
+        TexturePtr zp;
 };
 
 #endif
--- src/environmentmap.cpp
+++ src/environmentmap.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "environmentmap.h"
 #include "ray.h"
+#include "vectortypes.h"
 
 using namespace color;
 
@@ -8,10 +10,131 @@ ColorRGB EnvironmentMap::sampleRay(const Ray & ray)
     return ColorRGB::BLACK();
 }
 
+CubeMapEnvironmentMap::TexturePtr CubeMapEnvironmentMap::loadDirectionTile(const std::string & filename)
+{
+    auto texture = readImage<float>(filename);
+    if(!texture) {
+        throw 0;
+    }
+
+    texture->outOfBoundsBehavior = Texture::Repeat;
+
+    return texture;
+}
+
+bool CubeMapEnvironmentMap::loadFromDirectionFiles(
+    const std::string & xnFile,
+    const std::string & xpFile,
+    const std::string & ynFile,
+    const std::string & ypFile,
+    const std::string & znFile,
+    const std::string & zpFile)
+{
+    try {
+        xn = loadDirectionTile(xnFile);
+        xp = loadDirectionTile(xpFile);
+        yn = loadDirectionTile(ynFile);
+        yp = loadDirectionTile(ypFile);
+        zn = loadDirectionTile(znFile);
+        zp = loadDirectionTile(zpFile);
+        return true;
+    }
+    catch(std::exception & e) {
+        std::cout << "Caught exception: " << e.what() << std::endl;
+        return false;
+    }
+    catch(...) {
+        std::cout << "Caught unknown exception" << std::endl;
+        return false;
+    }
+}
+
+void CubeMapEnvironmentMap::directionToTileCoord(const Direction3 & v,
+                                                 TexturePtr & texture,
+                                                 TextureCoordinate & texcoord)
+{
+    float absX = std::abs(v.x);
+    float absY = std::abs(v.y);
+    float absZ = std::abs(v.z);
+
+    bool isXPositive = v.x > 0;
+    bool isYPositive = v.y > 0;
+    bool isZPositive = v.z > 0;
+
+    float maxAxis, uc, vc;
+
+    if (!isXPositive && absX >= absY && absX >= absZ) {
+        // u (0 to 1) goes from -z to +z
+        // v (0 to 1) goes from -y to +y
+        maxAxis = absX;
+        uc = v.z;
+        vc = v.y;
+        texture = xn;
+    }
+    if (isXPositive && absX >= absY && absX >= absZ) {
+        // u (0 to 1) goes from +z to -z
+        // v (0 to 1) goes from -y to +y
+        maxAxis = absX;
+        uc = -v.z;
+        vc = v.y;
+        texture = xp;
+    }
+    if (!isYPositive && absY >= absX && absY >= absZ) {
+        // u (0 to 1) goes from -x to +x
+        // v (0 to 1) goes from -z to +z
+        maxAxis = absY;
+        uc = v.x;
+        vc = v.z;
+        texture = yn;
+    }
+    if (isYPositive && absY >= absX && absY >= absZ) {
+        // u (0 to 1) goes from -x to +x
+        // v (0 to 1) goes from +z to -z
+        maxAxis = absY;
+        uc = v.x;
+        vc = -v.z;
+        texture = yp;
+    }
+    if (!isZPositive && absZ >= absX && absZ >= absY) {
+        // u (0 to 1) goes from +x to -x
+        // v (0 to 1) goes from -y to +y
+        maxAxis = absZ;
+        uc = -v.x;
+        vc = v.y;
+        texture = zn;
+    }
+    if (isZPositive && absZ >= absX && absZ >= absY) {
+        // u (0 to 1) goes from -x to +x
+        // v (0 to 1) goes from -y to +y
+        maxAxis = absZ;
+        uc = v.x;
+        vc = v.y;
+        texture = zp;
+    }
+
+    // Convert range from -1 to 1 to 0 to 1
+    texcoord.u = 0.5f * (uc / maxAxis + 1.0f);
+    texcoord.v = 0.5f * (vc / maxAxis + 1.0f);
+
+    // V runs bottom up in cube maps
+    texcoord.v = -texcoord.v;
+}
+
 ColorRGB CubeMapEnvironmentMap::sampleRay(const Ray & ray)
 {
+    TexturePtr texture;
+    TextureCoordinate texcoord;
+
+    directionToTileCoord(ray.direction, texture, texcoord);
+
+    //std::cout << "tex " << texcoord.u << ", " << texcoord.v << '\n'; // TEMP
+
+    return { texture->lerpUV(texcoord.u, texcoord.v, 0),
+             texture->lerpUV(texcoord.u, texcoord.v, 1),
+             texture->lerpUV(texcoord.u, texcoord.v, 2) };
+
     // TODO
-    return ColorRGB::RED();
+    //return ColorRGB::RED();
 }
 
 
