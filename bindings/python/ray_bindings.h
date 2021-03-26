13d3a755ae004620ff9c7014926bbde3bf1adced
David Cunningham <dacunni@gmail.com>
2021-03-25 23:18:29 -0600

    Added ray python bindings. Rearranged binding code

---
+++ bindings/python/ray_bindings.h
@@ -0,0 +1,44 @@
+#include <sstream>
+#include "Ray.h"
+
+void ray_bindings(py::module_ & m)
+{
+    py::class_<Ray>(m, "Ray")
+        // constructors
+        .def(py::init<>())
+        .def(py::init<const Position3 &, const Direction3 &>())
+        // methods
+        .def("pointAt", &Ray::pointAt)
+        // properties
+        .def_readwrite("origin", &Ray::origin)
+        .def_readwrite("direction", &Ray::direction)
+        // repr
+        .def("__repr__", [](const Ray & r) {
+                std::stringstream ss;
+                ss << r;
+                return ss.str();
+            });
+        ;
+
+    py::class_<RayIntersection>(m, "RayIntersection")
+        // constructors
+        .def(py::init<>())
+        // properties
+        .def_readwrite("ray", &RayIntersection::ray)
+        .def_readwrite("position", &RayIntersection::position)
+        .def_readwrite("normal", &RayIntersection::normal)
+        .def_readwrite("tangent", &RayIntersection::tangent)
+        .def_readwrite("bitangent", &RayIntersection::bitangent)
+        .def_readwrite("distance", &RayIntersection::distance)
+        .def_readwrite("material", &RayIntersection::material)
+        .def_readwrite("texcoord", &RayIntersection::texcoord)
+        .def_readwrite("hasTexCoord", &RayIntersection::hasTexCoord)
+        // repr
+        .def("__repr__", [](const RayIntersection & ri) {
+                std::stringstream ss;
+                ss << ri;
+                return ss.str();
+            });
+        ;
+}
+
