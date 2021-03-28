ea143ea173c35e3ad77de5880050176be53ff248
David Cunningham <dacunni@gmail.com>
2021-03-27 22:16:41 -0600

    Added python bindings for Sensor

--- bindings/python/pyfluxrt.cpp
+++ bindings/python/pyfluxrt.cpp
@@ -6,6 +6,7 @@ namespace py = pybind11;
 #include "vec3_bindings.h"
 #include "ray_bindings.h"
 #include "renderer_bindings.h"
+#include "sensor_bindings.h"
 
 PYBIND11_MODULE(pyfluxrt, m) {
     m.doc() = "Python bindings for fluxrt";
@@ -13,5 +14,6 @@ PYBIND11_MODULE(pyfluxrt, m) {
     vec3_bindings(m);
     ray_bindings(m);
     renderer_bindings(m);
+    sensor_bindings(m);
 }
 
---
+++ bindings/python/sensor_bindings.h
@@ -0,0 +1,27 @@
+#include <sstream>
+#include "sensor.h"
+
+void sensor_bindings(py::module_ & m)
+{
+    py::class_<Sensor>(m, "Sensor")
+        // constructors
+        .def(py::init<>())
+        .def(py::init<uint32_t, uint32_t>())
+        // methods
+        .def("forEachPixel", &Sensor::forEachPixel)
+        .def("forEachPixelInRect", &Sensor::forEachPixelInRect)
+        .def("forEachPixelTiled", &Sensor::forEachPixelTiled)
+        .def("forEachPixelThreaded", &Sensor::forEachPixelThreaded)
+        .def("forEachPixelTiledThreaded", &Sensor::forEachPixelTiledThreaded)
+        .def("pixelStandardImageLocation",
+             static_cast<vec2(Sensor::*)(float, float)>(&Sensor::pixelStandardImageLocation))
+        .def("pixelStandardImageLocation",
+             static_cast<vec2(Sensor::*)(const vec2 &)>(&Sensor::pixelStandardImageLocation))
+        .def("aspectRatio", &Sensor::aspectRatio)
+        .def("print", &Sensor::print)
+        // properties
+        .def_readwrite("pixelwidth", &Sensor::pixelwidth)
+        .def_readwrite("pixelheight", &Sensor::pixelheight)
+        ;
+}
+
