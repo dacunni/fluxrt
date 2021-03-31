b8d2bf044d9205d9e863a6def7557773357540d2
David Cunningham <dacunni@gmail.com>
2021-03-30 20:53:42 -0600

    more radiometry bindings

--- bindings/python/radiometry_bindings.h
+++ bindings/python/radiometry_bindings.h
@@ -5,6 +5,12 @@ void radiometry_bindings(py::module_ & m)
         // constructors
         .def(py::init<>())
         .def(py::init<float, float, float>())
+        .def_property("r", [](RadianceRGB & v) { return v.r; },
+                           [](RadianceRGB & v, float r) { v.r = r; })
+        .def_property("g", [](RadianceRGB & v) { return v.g; },
+                           [](RadianceRGB & v, float g) { v.g = g; })
+        .def_property("b", [](RadianceRGB & v) { return v.b; },
+                           [](RadianceRGB & v, float b) { v.b = b; })
         ;
 }
 
