10fd5a1d57d6641de8ca43822d11cfad0b5c2dd6
David Cunningham <dacunni@gmail.com>
2021-03-30 21:25:47 -0600

    Added RadianceRGB operator python bindings

--- bindings/python/radiometry_bindings.h
+++ bindings/python/radiometry_bindings.h
@@ -5,12 +5,20 @@ void radiometry_bindings(py::module_ & m)
         // constructors
         .def(py::init<>())
         .def(py::init<float, float, float>())
+        // properties
         .def_property("r", [](RadianceRGB & v) { return v.r; },
                            [](RadianceRGB & v, float r) { v.r = r; })
         .def_property("g", [](RadianceRGB & v) { return v.g; },
                            [](RadianceRGB & v, float g) { v.g = g; })
         .def_property("b", [](RadianceRGB & v) { return v.b; },
                            [](RadianceRGB & v, float b) { v.b = b; })
+        // operators
+        .def(py::self + py::self)
+        .def(py::self += py::self)
+        .def(py::self * float())
+        .def(py::self *= float())
+        .def(py::self / float())
+        .def(py::self /= float())
         ;
 }
 
