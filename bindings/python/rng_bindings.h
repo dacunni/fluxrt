7b85c26fafe40f6c54a0c223983c80aa54e500d1
David Cunningham <dacunni@gmail.com>
2021-04-10 21:31:43 -0600

    Added random notebook and python bindings for RNG

--- bindings/python/rng_bindings.h
+++ bindings/python/rng_bindings.h
@@ -5,6 +5,7 @@ void rng_bindings(py::module_ & m)
         // constructors
         .def(py::init<>())
         // methods
+        //   1D
         .def("uniform01", &RNG::uniform01)
         .def("uniformRange", &RNG::uniformRange)
         .def("gaussian", 
@@ -15,7 +16,47 @@ void rng_bindings(py::module_ & m)
              [](RNG & rng, float stddev) {
                 return rng.gaussian(stddev);
              })
-        // TODO
+        //   2D
+        .def("uniformUnitCircle",
+            [](RNG & rng) {
+                return rng.uniformUnitCircle();
+            })
+        .def("uniformCircle",
+            [](RNG & rng, float radius) {
+                return rng.uniformCircle(radius);
+            })
+        .def("uniformRectangle",
+            [](RNG & rng, float xmin, float xmax, float ymin, float ymax) {
+                return rng.uniformRectangle(xmin, xmax, ymin, ymax);
+            })
+        .def("gaussian2D", 
+             [](RNG & rng, float mean, float stddev) {
+                return rng.gaussian2D(mean, stddev);
+             })
+        .def("gaussian2D", 
+             [](RNG & rng, float stddev) {
+                return rng.gaussian2D(stddev);
+             })
+        .def("uniformSurfaceUnitSphere",
+             [](RNG & rng) {
+                return rng.uniformSurfaceUnitSphere();
+             })
+        .def("uniformSurfaceUnitHalfSphere",
+             [](RNG & rng, Direction3 & n) {
+                return rng.uniformSurfaceUnitHalfSphere(n);
+             })
+        .def("cosineAboutDirection",
+             [](RNG & rng, Direction3 & n) {
+                return rng.cosineAboutDirection(n);
+             })
+        .def("gaussian3D",
+             [](RNG & rng, float mean, float stddev) {
+                return rng.gaussian3D(mean, stddev);
+             })
+        .def("gaussian3D",
+             [](RNG & rng, float stddev) {
+                return rng.gaussian3D(stddev);
+             })
         ;
 }
 
